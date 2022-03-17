7d9f7a6724aee01ed1f7298f163271fba5513bda
ByeongHyeon <qudgus1984@naver.com>
2022-03-18 01:00:26 +0900

    구조체 완료

--- test.swift
+++ test.swift
@@ -1,5 +1,9 @@
-// 두 수를 입력받고, 입력받았을 때 두 수의 곱을 출력 하는 함수
-func x(num1: Int, num2: Int) -> Int {
-    return num1 * num2
+func addOne(left a: Int, right b: Int) -> Int {
+     return a + b
 }
-print(x(num1: 10, num2: 5))
+
+func addTwo(a: Int = 0, b: Int = 0) -> Int {
+     return a + b
+}
+
+addOne(a: 10, b: 20)
